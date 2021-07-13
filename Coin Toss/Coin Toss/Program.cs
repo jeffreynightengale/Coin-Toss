0689b10b0022fb1251a308c80c9d49504db6a72b
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-07-13 14:32:54 -0500

    finished

---
+++ Coin Toss/Coin Toss/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Coin_Toss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick heads or tails.");
+            string answer = Console.ReadLine();
+
+            int final;
+            if (answer.ToLower() == "heads")
+            {
+                final = 1;
+            }
+            else
+            {
+                final = 2;
+            }
+
+            Random rand = new Random();
+
+            int flip = rand.Next(1, 3);
+            
+
+            if (flip == 1)
+            {
+                Console.WriteLine("The coin landed on heads!");
+            }
+            else
+            {
+                Console.WriteLine("The coin landed on tails!");
+            }
+
+            if (final == flip)
+            {
+                Console.WriteLine("You guessed the correct flip!");
+            }
+            else
+            {
+                Console.WriteLine("You got it wrong, try again!");
+            }
+        }
+    }
+}
